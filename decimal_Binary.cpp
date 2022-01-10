140a1a31f515842913e7f53f1e31e0a96a174f6d
DnyaneshwarBakal <97299923+DnyaneshwarBakal@users.noreply.github.com>
2022-01-10 20:21:55 +0530

    Convert decimal number to binary bits

---
+++ decimal_Binary.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter a decimal nubmer";
+    cin >> n;
+    int i = 0;
+    int ans=0;
+    while (n != 0)
+    {
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        n = n >> 1;
+        i++;
+    }
+    cout  << ans;
+}
