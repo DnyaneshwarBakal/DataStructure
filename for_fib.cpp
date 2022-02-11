9009bebe5bf39eee9012c37a0ec677fc70f36ff7
DnyaneshwarBakal <97299923+DnyaneshwarBakal@users.noreply.github.com>
2022-02-11 19:21:08 +0530

    Add files via upload

---
+++ for_fib.cpp
@@ -0,0 +1,22 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+using  namespace std;
+int  main()
+{
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    int a=0;
+    int b=1;
+     cout<<a<<" "<<b<<" ";
+    for(int i=1;i<=n;i++)
+    {
+        int fib=a+b;
+        cout<<fib<<"  ";
+        a=b;
+        b=fib;
+    }
+
+
+ 
+}
